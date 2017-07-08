7abe2e9d98b92b68319758c764ca7f8ee1db872c
Damien <dasseya1@gmail.com>
2017-07-08 16:18:29 -0400

    Changed PORT from 3000 to env.PORT||3000

--- index.js
+++ index.js
@@ -31,4 +31,6 @@ const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Server is running on ' + PORT));
